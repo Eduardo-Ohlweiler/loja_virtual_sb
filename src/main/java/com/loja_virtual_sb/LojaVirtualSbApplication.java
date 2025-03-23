97ec1224a85c80104b62fcb6fcdfbce0247a4571
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-03-22 23:43:38 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/loja_virtual_sb/LojaVirtualSbApplication.java
@@ -0,0 +1,13 @@
+package com.loja_virtual_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualSbApplication.class, args);
+	}
+
+}
