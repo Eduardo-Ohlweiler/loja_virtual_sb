86ff1d62d3fed4b0ab0af54a98e0129860c3b06a
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2025-05-03 19:06:54 -0300

    Iniciando anotações no Application

--- src/main/java/com/loja_virtual_sb/LojaVirtualSbApplication.java
+++ src/main/java/com/loja_virtual_sb/LojaVirtualSbApplication.java
@@ -3,9 +3,15 @@ package com.loja_virtual_sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan("com.loja_virtual_sb.model")
+@ComponentScan("com.*")
+@EnableJpaRepositories(basePackages = "com.loja_virtual_sb.repository")
+@EnableTransactionManagement
 public class LojaVirtualSbApplication {
 
 	public static void main(String[] args) {
